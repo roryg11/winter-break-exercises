aeb6816ecac3818322f607b703f8a14e2d57e59b
roryg11 <Rory.C.Grant@gmail.com>
2014-12-30 16:28:40 -0700

    running command line app for reverse string

--- reverse_string/lib/reverse_string.rb
+++ reverse_string/lib/reverse_string.rb
@@ -1,11 +1,7 @@
 class ReverseString
-  def initialize(string)
-    @string = string
-  end
-
-  def reversed
-    string_swap_num = (@string.length)/2
-    rev_string = @string.split("")
+  def reversed(string)
+    string_swap_num = (string.length)/2
+    rev_string = string.split("")
     i = 0
     string_swap_num.times do
       rev_string[i], rev_string[0-(i + 1)] = rev_string[0   - (i+1)], rev_string[i]
@@ -13,4 +9,14 @@ class ReverseString
     end
     rev_string.join
   end
+
+  def run
+    puts "Enter a string:"
+    string = gets.chomp
+    reversed(string)
+  end
 end
+
+reverse_me = ReverseString.new
+
+puts reverse_me.run
