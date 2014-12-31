a6fd4bf0720e8b3ff3ab127a895d9298cc339b93
roryg11 <Rory.C.Grant@gmail.com>
2014-12-31 12:58:05 -0700

    taking out a few unnecessary methods

--- prime_numbers/lib/prime_number_printer.rb
+++ prime_numbers/lib/prime_number_printer.rb
@@ -1,15 +1,12 @@
 def prime_number_printer(max)
   num_range = (1..(max.to_i)).to_a.reverse
   not_prime = []
-  prime = []
   first_index = 0
   while first_index < (num_range.length - 1)
     second_index = 0
     while second_index < (num_range.length - 1)
         if num_range[first_index]%num_range[second_index] == 0 && num_range[first_index] != num_range[second_index] && num_range[second_index] != 1
           not_prime.push(num_range[first_index])
-        else
-          prime.push(num_range[first_index])
         end
       second_index += 1
     end
