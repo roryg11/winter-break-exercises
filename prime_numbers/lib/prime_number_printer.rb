f14db263be90b2c106ab56a23d6cc33ca82dc4c1
roryg11 <Rory.C.Grant@gmail.com>
2014-12-31 12:23:44 -0700

    working on method with testing

--- prime_numbers/lib/prime_number_printer.rb
+++ prime_numbers/lib/prime_number_printer.rb
@@ -1,3 +1,18 @@
 def prime_number_printer(max)
-  num_range = (1..29).to_a
+  num_range = (1..(max.to_i)).to_a.reverse
+
+  #outer_index_tracker
+  first_index = 0
+  while first_index < (num_range.length - 1)
+    #inner_index_tracker
+    second_index = 0
+    while second_index < (num_range.length - 1) 
+        if num_range[first_index]%num_range[second_index] == 0 && num_range[first_index] != num_range[second_index]
+          num_range.delete_at(first_index)
+        end
+      second_index += 1
+    end
+    first_index += 1
+  end
+  num_range
 end
--- prime_numbers/spec/prime_number_printer_spec.rb
+++ prime_numbers/spec/prime_number_printer_spec.rb
@@ -2,8 +2,11 @@ require "spec_helper"
 require "prime_number_printer"
 
 describe "#prime_number_printer(max)" do
-  it "returns an array of numbers that is a range from 1 to 'max'" do
-    expect(prime_number_printer(29)).to eq((1..29).to_a)
+  it "returns an array of prime numbers from the range" do
+
+    expected = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29].reverse
+
+    expect(prime_number_printer(29)).to eq(expected)
   end
 
 end
