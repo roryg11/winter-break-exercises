a59653f2b66bd6db43d625c347938c777cbd1ba7
roryg11 <Rory.C.Grant@gmail.com>
2014-12-31 12:56:36 -0700

    passing tests for prime_printer(max) method

--- prime_numbers/lib/prime_number_printer.rb
+++ prime_numbers/lib/prime_number_printer.rb
@@ -1,18 +1,26 @@
 def prime_number_printer(max)
   num_range = (1..(max.to_i)).to_a.reverse
-
-  #outer_index_tracker
+  not_prime = []
+  prime = []
   first_index = 0
   while first_index < (num_range.length - 1)
-    #inner_index_tracker
     second_index = 0
-    while second_index < (num_range.length - 1) 
-        if num_range[first_index]%num_range[second_index] == 0 && num_range[first_index] != num_range[second_index]
-          num_range.delete_at(first_index)
+    while second_index < (num_range.length - 1)
+        if num_range[first_index]%num_range[second_index] == 0 && num_range[first_index] != num_range[second_index] && num_range[second_index] != 1
+          not_prime.push(num_range[first_index])
+        else
+          prime.push(num_range[first_index])
         end
       second_index += 1
     end
     first_index += 1
   end
-  num_range
+  num_range.delete_if {|x| not_prime.include?(x)}
 end
+
+# reverse array so that you are only comparing the number with smaller numbers
+# keep track of the index of the index numbers
+# first_index keeps track of the index of your first number
+# second_index keeps track of the index of the numbers you compare it to
+# look for a remainder to know if your number is divisible by the number it is compared to
+# && statement is for when it is compared against itself
--- prime_numbers/spec/prime_number_printer_spec.rb
+++ prime_numbers/spec/prime_number_printer_spec.rb
@@ -2,9 +2,15 @@ require "spec_helper"
 require "prime_number_printer"
 
 describe "#prime_number_printer(max)" do
-  it "returns an array of prime numbers from the range" do
+  it "returns an array of prime numbers from a small range" do
 
-    expected = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29].reverse
+    expected = [1, 2, 3, 5, 7].reverse
+
+    expect(prime_number_printer(10)).to eq(expected)
+  end
+
+  it "returns and array of prime numbers from an array of 1 through 29" do
+    expected = [29, 23, 19, 17, 13, 11, 7, 5, 3, 2, 1]
 
     expect(prime_number_printer(29)).to eq(expected)
   end
