e3ee239fa356ae694ddc1a16f6005df6a3f1e378
roryg11 <Rory.C.Grant@gmail.com>
2015-01-01 21:27:00 -0700

    tests for Array class and methods count_evens and say_hello

--- screwing_with_arrays/lib/array.rb
+++ screwing_with_arrays/lib/array.rb
@@ -1,2 +1,15 @@
 class Array
+  def say_hello
+    "HEY THERE I'M AN ARRAY! SOMEONE FUCKED WITH MY CODE :("
+  end
+
+  def count_evens
+    count = 0
+    self.each do |num|
+      if num.even?
+        count += 1
+      end
+    end
+    "HI FRIEND, I AM ARRAY.  I HAVE #{count} EVEN NUMBERS.  PRETTY AWESOME YES?  I LOVE YOU GOODBYE"
+  end
 end
--- screwing_with_arrays/spec/array_spec.rb
+++ screwing_with_arrays/spec/array_spec.rb
@@ -2,7 +2,15 @@ require 'spec_helper'
 require 'array'
 
 describe Array do
-  it "works!" do
-    expect(true).to be(false)
+  it "#say_hello" do
+    num = [1,2, 3]
+
+    expect(num.say_hello).to eq("HEY THERE I'M AN ARRAY! SOMEONE FUCKED WITH MY CODE :(")
+  end
+
+  it "#count_evens" do
+    num = [2,5,9,15,22,99]
+
+    expect(num.count_evens).to eq("HI FRIEND, I AM ARRAY.  I HAVE 2 EVEN NUMBERS.  PRETTY AWESOME YES?  I LOVE YOU GOODBYE")
   end
 end
